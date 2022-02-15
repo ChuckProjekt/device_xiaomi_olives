d8dd5d4a4aa9a50c9d42ee064fcc3a4e189f0b6b
Chuck439 <firmankediri123@gmail.com>
2022-02-15 19:47:15 +0000

    olives: xdroid: Inherit pixel charging
    Signed-off-by: Chuck439 <firmankediri123@gmail.com>

--- xdroid_olives.mk
+++ xdroid_olives.mk
@@ -17,6 +17,7 @@ TARGET_SUPPORTS_GOOGLE_RECORDER := false
 TARGET_INCLUDE_STOCK_ARCORE := false
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
 TARGET_SUPPORTS_QUICK_TAP := true
+USE_PIXEL_CHARGING := true
 
 # Inherit from olives device
 $(call inherit-product, device/xiaomi/olives/device.mk)
