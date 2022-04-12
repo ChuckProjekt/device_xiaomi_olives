d3dbcef7d620dbb598ceb8a76de683eb2f2f4716
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-04-12 17:01:41 +0000

    olives: Drop product_launched_with_*.mk
    * It seems Google stopped making those
    * Directly set PRODUCT_SHIPPING_API_LEVEL
    
    Change-Id: If00badd6dc12e0245c74bba12e4816361091e754

--- aosp_olives.mk
+++ aosp_olives.mk
@@ -7,7 +7,9 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+
+# Set Shipping API level
+PRODUCT_SHIPPING_API_LEVEL := 28
 
 #
 # All components inherited here go to system_ext image
