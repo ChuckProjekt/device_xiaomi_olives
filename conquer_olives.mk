47478becf87abf4095e6ea995951957a26ddd7e1
Chuck439 <firmankediri123@gmail.com>
2022-08-05 21:08:43 +0000

    cqOS official
    Signed-off-by: Chuck439 <firmankediri123@gmail.com>

--- conquer_olives.mk
+++ conquer_olives.mk
@@ -12,6 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit some common conquerOS stuff.
 $(call inherit-product, vendor/conquer/config/common.mk)
 TARGET_BOOT_ANIMATION_RES := 720
+CONQUER_BUILD_TYPE := OFFICIAL
 
 # Inherit from olives device
 $(call inherit-product, device/xiaomi/olives/device.mk)
